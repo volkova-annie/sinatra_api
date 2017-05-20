35a67814590d5848d459ebaf21907fdc90e33bfe
Anna Volkova <annie_v@bk.ru>
2017-05-20 13:30:53 +0300

    add connection to db

--- my_sinatra.rb
+++ my_sinatra.rb
@@ -1,19 +1,40 @@
 require 'sinatra'
 require 'sinatra/namespace'
 require 'sinatra/base'
+require 'sequel'
+require 'sequel/extensions/seed'
+require 'pg'
 
-module MyAppModule
-  class App < Sinatra::Base
-    register Sinatra::Namespace
+DB = Sequel.connect(
+    adapter: :postgres,
+    database: 'sinatra_seq_dev',
+    host: 'localhost',
+    password: 'password',
+    user: 'annie',
+    max_connections: 10,
+# logger: Logger.new('log/db.log')
+)
 
+Sequel::Seed.setup :development # Set the environment
+Sequel.extension :seed # Load the extension
+Sequel::Seeder.apply(DB, './seeds') # Apply the seeds/fixtures
+
+%w{controllers models routes}.each {|dir| Dir.glob("./#{dir}/*.rb", &method(:require))}
+
+
+
+# module MyAppModule
+#   class App < Sinatra::Base
+#     register Sinatra::Namespace
+#
       get '/' do
-        # 'Hello My Sinatrra - Easy and Wide World!'
+        'Hello My Sinatrra - Easy and Wide World!'
         redirect to('/hello/World')
       end
 
       get '/hello/:name' do |n|
-        # "Sinatra приветствует тебя, #{params[:name]}"
-        # "Hello #{params['name']}"
+        "Sinatra приветствует тебя, #{params[:name]}"
+        "Hello #{params['name']}"
         "Hello #{n}!"
       end
 
@@ -21,10 +42,20 @@ module MyAppModule
         "Hello, #{c}!"
       end
 
+      get '/say/*/to/*' do
+        #/say/hello/to/world
+        params['splat'].to_s #=> ["hello","world"]
+      end
+
+      get '/jobs.?:format?' do
+        # "GET/jobs", "GET/jobs.json", "GET/jobs.xml"
+        'Маршрут работает'
+      end
+
       namespace '/api/v1' do
         get '/*' do
           "I don't know what is the #{params[:splat]}. It's what you typed"
         end
       end
-  end
-end
+#   end
+# end
